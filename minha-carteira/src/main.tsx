01a2e84bddd7893c9dc5f86f927f854b0fd7b3e7
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-14 19:57:12 +0100

    estilos globais com styled components

--- minha-carteira/src/main.tsx
+++ minha-carteira/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Dashboard from './pages/Dashboard'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Dashboard />
+    <App />
   </StrictMode>,
 )
