84a4fa20deed68fb776d0e77a0e1718f41efff1a
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-14 23:54:50 +0100

    criando header principal

--- minha-carteira/src/components/MainHeader/index.tsx
+++ minha-carteira/src/components/MainHeader/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import {Container} from './styles'
+import React, {useMemo} from "react";
+import {Container, Profile,Welcome , UserName} from './styles'
+import emojis from "../../utils/emojis";
+
 
 const MainHeader : React.FC = () => {
+
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length)
+    return emojis[indice]
+  }, [])
+
+
   return(
     <Container>
-     <h1>MainHeader</h1>
+     <h1>Toogle</h1>
+
+     <Profile>
+      <Welcome>Olaá, {emoji}</Welcome>
+      <UserName>Tiago Sousa</UserName>
+     </Profile>
     </Container>
   )
 }
